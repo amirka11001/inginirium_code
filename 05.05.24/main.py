e01a36510e969a4de7cffabcb94e12be93b4a99b
nikita58-hub <nikitayt954@gmail.com>
2024-05-12 17:48:02 +0400

    игра

--- 05.05.24/main.py
+++ 05.05.24/main.py
@@ -20,6 +20,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.y=400
         self.health=3
+        self.to_left = False
+        self.to_right = True
 
     # Создаем функцию обновления спрайта
     def update(self):
@@ -30,9 +32,18 @@ class Player(pygame.sprite.Sprite):
     def move_by_keys(self):
         keys = pygame.key.get_pressed() # Получаем все нажатые клавиши
         if keys[pygame.K_a]: # Если нажата клавиша A
+            self.image = pygame.image.load('капхед2.png')
+            self.image = pygame.transform.scale(self.image, (110, 150))
             self.rect.left -= 10 # Двигаемся влево на 5 пикселей
+            self.to_left = True
+            self.to_right = False
         if keys[pygame.K_d]: # Если нажата клавиша D
-            self.rect.left += 10
+
+            self.image = pygame.image.load('капхед.png')
+            self.image = pygame.transform.scale(self.image, (110, 150))
+            self.rect.left += 7
+            self.to_right = True
+            self.to_left = False
         if keys[pygame.K_SPACE]:
             self.is_jump=True
         if self.is_jump:
@@ -62,20 +73,24 @@ class Enemy(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (100, 100))
         # Задаем границы
         self.rect = self.image.get_rect()
-        self.rect.y = 470
         self.dir = 'right'
         self.health = 5
     def update(self):
         if self.rect.right>width:
-            #self.rect.left-=5
+            self.image = pygame.image.load('враг.png')
+            self.image = pygame.transform.scale(self.image, (100, 100))
             self.dir = 'left'
+            self.rect.x = 900
         if self.rect.left<0:
             self.dir = 'right'
+            self.image = pygame.image.load('враг2.png')
+            self.image = pygame.transform.scale(self.image, (100, 100))
             #self.rect.left+=5
         if self.dir == 'right':
-            self.rect.left += 5
+            self.rect.left += 6
         elif self.dir == 'left':
-            self.rect.left -= 5
+            self.rect.left -= 6
+        self.rect.y = 470
 
 class Bullet(pygame.sprite.Sprite):
     # Создаем конструктор
@@ -89,12 +104,47 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centery=player.rect.centery
         self.rect.left=player.rect.right
+        self.to_left = False
+        self.to_right = True
     def update(self):
-        self.rect.right+=5
+        if self.to_right:
+            self.rect.right+=5
+        elif self.to_left:
+            self.rect.left -= 5
+
+
+
+
+class Bird(pygame.sprite.Sprite):
+    # Создаем инициализатор(конструктор)
+    def __init__(self):
+        # Вызываем конструктор самого класса Sprite
+        super().__init__()
+        # Загружаем изображение
+        self.image = pygame.image.load('Птица.png')
+        # Задаем размер. Первая 100 - ширина, вторая 100 - высота
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        # Задаем границы
+        self.rect = self.image.get_rect()
+        self.rect.y = 100
+        self.dir = 'right'
+        self.health = 5
+
+    def update(self):
+        if self.rect.right>width:
+            #self.rect.left-=5
+            self.dir = 'left'
+        if self.rect.left<0:
+            self.dir = 'right'
+            #self.rect.left+=5
+        if self.dir == 'right':
+            self.rect.left += 1
+        elif self.dir == 'left':
+            self.rect.left -= 1
 pygame.init()
 
 # Создаем переменные ширины и высоты экрана
-width = 800
+width = 1000
 height = 600
 
 # Создаем окно с заранее созданными размерами
@@ -109,14 +159,15 @@ background = pygame.transform.scale(background, (width, height))
 # Создаем группу спрайтов
 all_sprites = pygame.sprite.Group()
 enemy = Enemy()
+bird=Bird()
 player = Player(all_sprites)
 
 enemy.rect.left = 200
 enemy_sprites = pygame.sprite.Group()
-
+bird_sprites=pygame.sprite.Group()
 # Добавляем противника в группу спрайтов противников
 enemy_sprites.add(enemy)
-
+bird_sprites.add(bird)
 bullet_sprites = pygame.sprite.Group()
 
 # Создаем переменную FPS
@@ -131,10 +182,11 @@ text = font.render('Жизни:' + str(player.health), True, (255, 255, 255))
 
 text_enemy = font.render('Жизни:' + str(enemy.health), True, (255, 255, 255))
 
-#sound = pygame.mixer.Sound('6d91dda56e9b0c1.mp3')
-#sound.play()
+
 # Бесконечные игровой цикл
 d=1
+sound = pygame.mixer.Sound('мелодия.mp3')
+sound.play()
 while True:
     # Перебираем все возможные события
     for event in pygame.event.get():
@@ -146,6 +198,18 @@ while True:
             if d < 20:
                 if event.key == pygame.K_f:
                     bullet = Bullet()
+                    if player.to_right:
+                        bullet.to_right = True
+                        bullet.to_left = False
+
+                        bullet.image = pygame.image.load('пуля.png')
+                        bullet.image = pygame.transform.scale(bullet.image, (50, 50))
+                    elif player.to_left:
+                        bullet.to_left = True
+                        bullet.to_right = False
+
+                        bullet.image = pygame.image.load('пуля2.png')
+                        bullet.image = pygame.transform.scale(bullet.image, (50, 50))
                     bullet_sprites.add(bullet)
     d+=1
     if d == 60:
@@ -170,10 +234,11 @@ while True:
 
 
     enemy_sprites.draw(win)
-
     enemy_sprites.update()
     bullet_sprites.draw(win)
     bullet_sprites.update()
+    bird_sprites.draw(win)
+    bird_sprites.update()
     # Получаем столкновения(список спрайтов, с которыми столкнулся персонаж)
     hits = pygame.sprite.spritecollide(player,enemy_sprites, False)
     # Если этот список не пустой
@@ -189,7 +254,7 @@ while True:
 
     if enemy.health <=0:
         enemy.health = 5
-        enemy.rect.right = width
+        enemy.rect.right = width+140
     if player.health <= 0:
        break
 
